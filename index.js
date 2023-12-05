8f1136ed35dd4b5dcc275ec59cf4753302ab569b
shivam2403 <136799994+shivam2403@users.noreply.github.com>
2023-12-05 21:49:31 +0530

    Update index.js

--- index.js
+++ index.js
@@ -17,6 +17,12 @@ app.use(cors(
     }
 ));
 
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    next();
+  });
+
 dotenv.config();
 mongoose.connect(process.env.MONGO_URL);
 
