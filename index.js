ebb37ca5981cea4089a157ad6ecf091cbcde5fe0
shivam2403 <136799994+shivam2403@users.noreply.github.com>
2023-12-07 01:25:32 +0530

    Update index.js

--- index.js
+++ index.js
@@ -9,19 +9,14 @@ const listRouter=require('./routes/lists');
 const cors=require('cors');
 
 
-app.use(cors(
-    {
-        origin: [""],
-        methods: ["POST","GET"],
-        credentials: true
-    }
-));
+app.options('*', cors());
 
-app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
-    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-    next();
-  });
+app.use(
+    cors({
+        origin: "*",
+        credentials: true,
+    })
+  )
 
 dotenv.config();
 mongoose.connect(process.env.MONGO_URL);
